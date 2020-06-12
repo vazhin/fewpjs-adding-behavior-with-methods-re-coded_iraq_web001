77df9b4114e6197e5b9ae3547330c047bd3f8e9f
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-12 21:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ class Cat {
    this.name = name;
    this.sex = sex;
  }
+ speak() {
+   return `${this.name} says meow!`
+ }
 }
 
 class Dog {
   constructor (name, sex) {
     this.name = name;
     this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof!`
   }
 }
 
