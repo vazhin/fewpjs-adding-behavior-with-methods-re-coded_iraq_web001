3712845b74c04be49548e491a047bbb9442b307a
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-12 21:31:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ class Cat {
 }
 
 class Dog {
-
+  constructor (name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
 }
 
 class Bird {
-
+  constructor (name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
 }
